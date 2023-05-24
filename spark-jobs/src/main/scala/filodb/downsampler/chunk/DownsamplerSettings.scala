18c7cbdaf8e89e802987ee47c716bb3ea6e80cd7
Abhishek <abhishekraj10@outlook.com>
2023-05-24 17:43:19 +0100

    Add chunk-persistor-splits param.

--- spark-jobs/src/main/scala/filodb/downsampler/chunk/DownsamplerSettings.scala
+++ spark-jobs/src/main/scala/filodb/downsampler/chunk/DownsamplerSettings.scala
@@ -82,6 +82,7 @@ class DownsamplerSettings(conf: Config = ConfigFactory.empty()) extends Serializ
 
   @transient lazy val shouldUseChunksPersistor = downsamplerConfig.getBoolean("use-chunks-persistor")
   @transient lazy val chunksPersistor = downsamplerConfig.getString("chunks-persistor")
+  @transient lazy val chunkPersistorSplits = downsamplerConfig.getString("chunks-persistor-splits")
 
   @transient lazy val exportRuleKey = downsamplerConfig.as[Seq[String]]("data-export.key-labels")
 
